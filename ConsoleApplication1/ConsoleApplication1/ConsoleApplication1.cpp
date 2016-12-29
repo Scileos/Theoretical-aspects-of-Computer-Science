9490eb50402992fbcc6f5d1c2076a2fccc8c1945
Jonathan <00Jonniem@gmail.com>
2016-12-29 00:14:37 +0000

    VS project setup, task 1 comment code

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+
+int main()
+{
+	string test;
+
+	cout << "Hello World";
+}
+
